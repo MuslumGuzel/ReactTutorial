0b7c1f355f66b3c893b2b209e59f6d8370e02729
MuslumGuzel <guzel.muslum8@gmail.com>
2020-04-05 00:38:59 +0300

    - index js fixed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App1 from './App1';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App1 />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
